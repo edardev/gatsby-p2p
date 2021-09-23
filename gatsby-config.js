5965a2d3253f86b3ba36d92299c38d611be9a6d2
EAdeveloper <edward@menteware.com>
2021-09-22 23:11:32 -0400

    Add Gatsby with @mui/material and @emotion dependencies

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-top-layout",
+    "gatsby-plugin-react-helmet",
+    // If you want to use styled components you should add the plugin here.
+    // 'gatsby-plugin-styled-components',
+    "gatsby-plugin-mui-emotion",
+  ],
+  siteMetadata: {
+    title: "P2P App",
+  },
+};
